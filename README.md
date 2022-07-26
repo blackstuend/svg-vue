20e68e01cfb9eb44c80d723b55c6c5365cf55adf
blackfloat <lin70208gmail.com>
2022-07-26 22:27:00 +0800

    feat: 新增圖片引入方式

--- README.md
+++ README.md
@@ -1,24 +1,7 @@
-# svg-vue
+# Vue 使用 svg 的幾種做法
 
-## Project setup
-```
-npm install
-```
+## Can I use
+* https://caniuse.com/?search=filter
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## 引用
+* https://codepen.io/sosuke/pen/Pjoqqp
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Icon</h1>
+      <img src="@/assets/icons/user.svg" class="icon-user">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
@@ -24,5 +20,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.icon-user {
+  filter: invert(55%) sepia(31%) saturate(4362%) hue-rotate(158deg) brightness(90%) contrast(101%);
+
+  &:hover {
+    filter: invert(0%) sepia(0%) saturate(7489%) hue-rotate(297deg) brightness(96%) contrast(104%);
+  }
 }
 </style>
